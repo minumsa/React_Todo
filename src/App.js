88bc6e1ea33292312532b31a03ae7ecdcc3c8c58
minumsa <icycyi@naver.com>
2022-07-28 21:11:53 +0900

    Drag and Drop(solved)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Form from './components/Form';
-import List from "./components/List";
+import Form from "./components/Form";
+import Lists from "./components/Lists";
 
-export default function App() { // eslint-disable-next-line 
-  const state = { 
-    todoData : [],
-    value: ""
-  }; 
+export default function App() {
+  // eslint-disable-next-line
+  const state = {
+    todoData: [],
+    value: "",
+  };
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
 
-  const handleSubmit = (e) => { // eslint-disable-next-line
+  const handleSubmit = e => {
+    // eslint-disable-next-line
     e.preventDefault();
 
     //새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
       title: value,
-      completed: false
+      completed: false,
     };
 
     //원래 있던 할 일에 새로운 할 일 더해주기
-    setTodoData((prev) => [...prev, newTodo]);
+    setTodoData(prev => [...prev, newTodo]);
     setValue("");
   };
-  
-  return(
+
+  return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
         </div>
-        <List todoData={todoData} setTodoData={setTodoData}/>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
 
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
-  ); 
-};
+  );
+}
